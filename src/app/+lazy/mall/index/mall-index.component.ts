3fdddc89b46fbd126d82bff0abcac6c5b541cb69
XIE7654 <765462425@qq.com>
2017-09-22 15:14:37 +0800

    add:修改轮播图

--- src/app/+lazy/mall/index/mall-index.component.ts
+++ src/app/+lazy/mall/index/mall-index.component.ts
@@ -1,19 +1,19 @@
 import {
-  Component, OnInit, ElementRef, OnDestroy, HostListener, Renderer2, ViewEncapsulation,
-  AfterViewInit
+  Component, OnInit, ElementRef, OnDestroy, Renderer2, AfterViewInit
 } from '@angular/core';
 import { ProductService } from '../../../shared/services/products/product.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Apply } from '../../../shared/utils/models/Apply';
 import { ArticleService } from '../../../shared/services/article/article.service';
 import { Carousel } from '../../../shared/common/header/carousel/carousel';
+declare const Swiper: any;
 
 @Component({
   selector: 'mall-index',
   templateUrl: './mall-index.component.html',
-  styleUrls: ['./mall-index.component.scss'],
+  styleUrls: ['./swiper.min.css', './mall-index.component.scss'],
 })
-export class MallIndexComponent implements OnInit, OnDestroy, AfterViewInit, OnDestroy {
+export class MallIndexComponent implements OnInit, AfterViewInit, OnDestroy {
   public isFirst: string = 'true';
   public promotiones;
   public window_width;
@@ -55,20 +55,46 @@ export class MallIndexComponent implements OnInit, OnDestroy, AfterViewInit, OnD
         this.isFirst = 'false';
       }
       this.onBom();
+      this.getBanner();
     }
-    this.getBanner();
+
+  }
+  ngOnDestroy(): void {
+    if (typeof window !== 'undefined') {
+      document.getElementsByTagName('head')[0].removeChild(document.getElementById('swiper'));
+    }
   }
   ngAfterViewInit(): void {
-    if (typeof window !== 'undefined' && document.querySelector('.mengban')) {
-      const mengban = document.querySelector('.mengban');
-      mengban.addEventListener('touchstart', this.end, false);
+    if (typeof window !== 'undefined') {
+      if (document.querySelector('.mengban')) {
+        const mengban = document.querySelector('.mengban');
+        mengban.addEventListener('touchstart', this.end, false);
+      }
     }
+  }
+  /**
+   * swiper.min.js
+   * @method loadScript
+   */
+  loadScript(url, id, callback) {
+    const script = document.createElement ('script');
+    script.type = 'text/javascript';
+    script.id = id;
+    script.onload = function() {
+      callback();
+    };
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
   }
   getBanner() {
     this.articleservice.getArticleBaner().then((res) => {
-      this.article_banneres = res.reverse();
-      this.myObjects = this.article_banneres.map((obj) => {
-        return new Carousel(obj.titleImg, obj.id);
+      this.article_banneres = res;
+      this.loadScript('assets/lib/swiper.min.js', 'swiper', () => {
+        const swiper = new Swiper('.swiper-container', {
+          autoplay: 2500,
+          autoplayDisableOnInteraction: false,
+          loop: true
+        });
       });
     });
   }
@@ -78,7 +104,6 @@ export class MallIndexComponent implements OnInit, OnDestroy, AfterViewInit, OnD
   goProducts(e) {
     this.router.navigate(['/product']);
   }
-  ngOnDestroy() {}
   isWoman() {
     if (typeof window !== 'undefined' && document.querySelector('.mengban')) {
       const mengban = document.querySelector('.mengban');
@@ -87,7 +112,6 @@ export class MallIndexComponent implements OnInit, OnDestroy, AfterViewInit, OnD
     this.isFirst = 'false';
     localStorage.setItem('isFirst', 'false');
     localStorage.setItem('isMan', 'false');
-
   }
   hideMengban() {
     if (typeof window !== 'undefined' && document.querySelector('.mengban')) {
